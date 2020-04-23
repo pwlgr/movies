29162cd54fc6f7e1903fd47bb099185bac8ceec5
Paweł Waligóra <pawelwaligora1337@gmail.com>
2020-04-23 21:20:29 +0200

    add cors support

--- api/app.js
+++ api/app.js
@@ -8,6 +8,17 @@ const { Genre, Movie } = require('./db/models');
 
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+	if (req.method === 'OPTIONS') {
+		res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+		res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
+		return res.status(200).json({});
+	}
+	next();
+});
+
 app.get('/genres', (req, res) => {
 	Genre.find({}).then((genres) => {
 		res.send(genres);
