88485c0a138400e92139c708b2db92b5ddddd2d6
Paweł Waligóra <pawelwaligora1337@gmail.com>
2020-05-03 00:21:58 +0200

    fix retrieveing tokens from api

--- api/app.js
+++ api/app.js
@@ -11,6 +11,8 @@ app.use(bodyParser.json());
 app.use(function(req, res, next) {
 	res.header('Access-Control-Allow-Origin', '*');
 	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+	res.header('Access-Control-Expose-Headers', 'x-access-token, x-refresh-token');
+
 	if (req.method === 'OPTIONS') {
 		res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
 		res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
